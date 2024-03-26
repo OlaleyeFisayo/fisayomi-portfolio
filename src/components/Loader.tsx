1f40af614b6319435859639f27cc62f97e1e53ed
Festus-Olaleye Oluwafisayomi Oluwaseunfunmi <festusolaleyef@gmail.com>
2024-03-26 15:19:03 +0100

    fixed: loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,20 +1,19 @@
 import InfiniteScroller from "./InfiniteScroller";
 
 export default function Loader() {
-  const scrollerArray = new Array(18).fill("");
+  const scrollerArray = new Array(16).fill(" ");
   const currentYear = new Date().getFullYear();
 
   return (
     <section
       id="loader"
-      className="h-screen w-screen bg-loader text-offWhite absolute z-30"
+      className="h-screen w-screen bg-black text-offWhite absolute z-30 shutter"
     >
-      <div className="fixed h-screen w-[550vw] md:w-[150vw] flex-nowrap overflow-hidden flex-col rotate-[315deg] top-[-100%] left-[-200%] z-30 md:top-[-25%] md:left-[-80%]">
+      <div className="fixed h-screen w-[550vw] md:w-[200vw] flex-nowrap flex-col rotate-[315deg] top-[-100%] left-[-250%] z-30 md:top-[-25%] md:left-[-80%]">
         {scrollerArray.map((items, index) => {
           return (
             <InfiniteScroller
               key={index}
-              delay={`${index}00ms`}
               direction={`${index % 2 === 0 ? "right" : "left"}`}
             />
           );
@@ -22,10 +21,12 @@ export default function Loader() {
       </div>
 
       <div className=" w-screen h-screen flex justify-center items-center px-1 py-1">
-        <h1 className="text-center text-4xl z-40">
+        <h1 className="text-center text-4xl z-40" id="title-1">
           OlaleyeFisayo <span className="font-extrabold">@{currentYear}</span>{" "}
           <br />
-          <p className="text-center text-4xl z-40">Demofolio</p>
+          <p className="text-center text-4xl z-40" id="title-2">
+            Demofolio
+          </p>
         </h1>
       </div>
     </section>
