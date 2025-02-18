61117a381c62c8b68452ccaa671747f3f779e7de
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-02-18 20:54:09 +0100

    basic setup

---
+++ src/components/section/my-stack.tsx
@@ -0,0 +1,13 @@
+import { Layers } from "lucide-react"
+import AppCard from "../ui/app-card"
+import { CardHeader } from "../ui/card"
+
+export default function MyStack() {
+  return (
+    <AppCard>
+        <CardHeader>
+            <Layers />
+        </CardHeader>
+    </AppCard>
+  )
+}
